1d23054586a3c164914b9245983fe8fd4f2ffc66
john kaiser <52598487+johnkaizer@users.noreply.github.com>
2022-06-06 18:14:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notesapp/NotesRepository.kt
@@ -0,0 +1,19 @@
+package com.example.notesapp
+
+import androidx.lifecycle.LiveData
+
+class NotesRepository(private val notesDao: NotesDao) {
+    val allNotes: LiveData<List<Note>> = notesDao.getAllNotes()
+
+    suspend fun insert(note: Note) {
+        notesDao.insert(note)
+    }
+
+    suspend fun delete(note: Note) {
+        notesDao.delete(note)
+    }
+
+    suspend fun update(note: Note) {
+        notesDao.update(note)
+    }
+}
